969b4a36f4a4b426ab0f6493e19a34089c804e3a
Sebastian Rodriguez <sebastian114224@gmail.com>
2023-03-16 16:04:45 -0400

    variable type switched t odate in inventory table for the due date and manufacture date

---
+++ InventoryManagmentSystem/Database/dbo.tbInventory.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tbInventory] (
+    [Id]               INT           NOT NULL,
+    [Item Type]        VARCHAR (50)  NULL,
+    [Serial Number]    INT           NULL,
+    [Brand]            NVARCHAR (50) NULL,
+    [Size]             FLOAT (53)    NULL,
+    [Manufacture Date] DATE  NULL,
+    [Due Date]         DATE  NULL,
+    [Location]         VARCHAR (50)  NULL,
+    [Color]            NVARCHAR (50) NULL,
+    [Rubber / Leather] VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
