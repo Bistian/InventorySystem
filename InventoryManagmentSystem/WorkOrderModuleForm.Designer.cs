1488463edecb475c2a1195bd8f4972fe2a2d08cb
FreyLF <fernandoo_b@hotmail.com>
2023-04-28 12:12:54 -0300

    Working on printing

--- InventoryManagmentSystem/WorkOrderModuleForm.Designer.cs
+++ InventoryManagmentSystem/WorkOrderModuleForm.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkOrderModuleForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.NewJacketTxt = new System.Windows.Forms.Label();
-            this.CloseButton = new InventoryManagmentSystem.CustomButton();
             this.MyLogo = new System.Windows.Forms.PictureBox();
             this.LableClientName = new System.Windows.Forms.Label();
             this.lableRentalOption = new System.Windows.Forms.Label();
@@ -41,9 +40,13 @@
             this.Item4 = new System.Windows.Forms.Label();
             this.lablePrice = new System.Windows.Forms.Label();
             this.labelamount = new System.Windows.Forms.Label();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.CloseButton = new InventoryManagmentSystem.CustomButton();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MyLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,9 +57,8 @@
             this.panel1.Controls.Add(this.MyLogo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 90);
+            this.panel1.Size = new System.Drawing.Size(600, 73);
             this.panel1.TabIndex = 1;
             // 
             // NewJacketTxt
@@ -68,35 +70,19 @@
             this.NewJacketTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NewJacketTxt.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.NewJacketTxt.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.NewJacketTxt.Location = new System.Drawing.Point(296, 27);
-            this.NewJacketTxt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NewJacketTxt.Location = new System.Drawing.Point(222, 22);
             this.NewJacketTxt.Name = "NewJacketTxt";
-            this.NewJacketTxt.Size = new System.Drawing.Size(179, 36);
+            this.NewJacketTxt.Size = new System.Drawing.Size(148, 29);
             this.NewJacketTxt.TabIndex = 17;
             this.NewJacketTxt.Text = "Work Order";
             this.NewJacketTxt.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // CloseButton
-            // 
-            this.CloseButton.Image = ((System.Drawing.Image)(resources.GetObject("CloseButton.Image")));
-            this.CloseButton.ImageHover = ((System.Drawing.Image)(resources.GetObject("CloseButton.ImageHover")));
-            this.CloseButton.ImageNormal = ((System.Drawing.Image)(resources.GetObject("CloseButton.ImageNormal")));
-            this.CloseButton.Location = new System.Drawing.Point(751, 11);
-            this.CloseButton.Margin = new System.Windows.Forms.Padding(4);
-            this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(36, 42);
-            this.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.CloseButton.TabIndex = 13;
-            this.CloseButton.TabStop = false;
-            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
-            // 
             // MyLogo
             // 
             this.MyLogo.Image = ((System.Drawing.Image)(resources.GetObject("MyLogo.Image")));
-            this.MyLogo.Location = new System.Drawing.Point(4, 4);
-            this.MyLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.MyLogo.Location = new System.Drawing.Point(3, 3);
             this.MyLogo.Name = "MyLogo";
-            this.MyLogo.Size = new System.Drawing.Size(85, 82);
+            this.MyLogo.Size = new System.Drawing.Size(64, 67);
             this.MyLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.MyLogo.TabIndex = 6;
             this.MyLogo.TabStop = false;
@@ -105,9 +91,10 @@
             // 
             this.LableClientName.AutoSize = true;
             this.LableClientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LableClientName.Location = new System.Drawing.Point(323, 133);
+            this.LableClientName.Location = new System.Drawing.Point(242, 108);
+            this.LableClientName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LableClientName.Name = "LableClientName";
-            this.LableClientName.Size = new System.Drawing.Size(139, 51);
+            this.LableClientName.Size = new System.Drawing.Size(112, 39);
             this.LableClientName.TabIndex = 2;
             this.LableClientName.Text = "Client";
             // 
@@ -115,9 +102,10 @@
             // 
             this.lableRentalOption.AutoSize = true;
             this.lableRentalOption.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lableRentalOption.Location = new System.Drawing.Point(63, 229);
+            this.lableRentalOption.Location = new System.Drawing.Point(47, 186);
+            this.lableRentalOption.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lableRentalOption.Name = "lableRentalOption";
-            this.lableRentalOption.Size = new System.Drawing.Size(205, 36);
+            this.lableRentalOption.Size = new System.Drawing.Size(166, 29);
             this.lableRentalOption.TabIndex = 3;
             this.lableRentalOption.Text = "Rental Option:";
             // 
@@ -125,9 +113,10 @@
             // 
             this.Item1.AutoSize = true;
             this.Item1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Item1.Location = new System.Drawing.Point(127, 279);
+            this.Item1.Location = new System.Drawing.Point(95, 227);
+            this.Item1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Item1.Name = "Item1";
-            this.Item1.Size = new System.Drawing.Size(72, 29);
+            this.Item1.Size = new System.Drawing.Size(55, 24);
             this.Item1.TabIndex = 5;
             this.Item1.Text = "Item1";
             // 
@@ -135,9 +124,10 @@
             // 
             this.Item2.AutoSize = true;
             this.Item2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Item2.Location = new System.Drawing.Point(127, 312);
+            this.Item2.Location = new System.Drawing.Point(95, 254);
+            this.Item2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Item2.Name = "Item2";
-            this.Item2.Size = new System.Drawing.Size(72, 29);
+            this.Item2.Size = new System.Drawing.Size(55, 24);
             this.Item2.TabIndex = 6;
             this.Item2.Text = "Item2";
             // 
@@ -145,9 +135,10 @@
             // 
             this.Item3.AutoSize = true;
             this.Item3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Item3.Location = new System.Drawing.Point(127, 346);
+            this.Item3.Location = new System.Drawing.Point(95, 281);
+            this.Item3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Item3.Name = "Item3";
-            this.Item3.Size = new System.Drawing.Size(72, 29);
+            this.Item3.Size = new System.Drawing.Size(55, 24);
             this.Item3.TabIndex = 7;
             this.Item3.Text = "Item3";
             // 
@@ -155,9 +146,10 @@
             // 
             this.Item4.AutoSize = true;
             this.Item4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Item4.Location = new System.Drawing.Point(127, 378);
+            this.Item4.Location = new System.Drawing.Point(95, 307);
+            this.Item4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Item4.Name = "Item4";
-            this.Item4.Size = new System.Drawing.Size(72, 29);
+            this.Item4.Size = new System.Drawing.Size(55, 24);
             this.Item4.TabIndex = 8;
             this.Item4.Text = "Item4";
             // 
@@ -165,9 +157,10 @@
             // 
             this.lablePrice.AutoSize = true;
             this.lablePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lablePrice.Location = new System.Drawing.Point(418, 229);
+            this.lablePrice.Location = new System.Drawing.Point(314, 186);
+            this.lablePrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lablePrice.Name = "lablePrice";
-            this.lablePrice.Size = new System.Drawing.Size(108, 36);
+            this.lablePrice.Size = new System.Drawing.Size(89, 29);
             this.lablePrice.TabIndex = 9;
             this.lablePrice.Text = "Price =";
             // 
@@ -175,18 +168,51 @@
             // 
             this.labelamount.AutoSize = true;
             this.labelamount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelamount.Location = new System.Drawing.Point(532, 229);
+            this.labelamount.Location = new System.Drawing.Point(399, 186);
+            this.labelamount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelamount.Name = "labelamount";
-            this.labelamount.Size = new System.Drawing.Size(113, 36);
+            this.labelamount.Size = new System.Drawing.Size(92, 29);
             this.labelamount.TabIndex = 10;
             this.labelamount.Text = "amount";
             // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.BackColor = System.Drawing.Color.Maroon;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnPrint.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnPrint.Location = new System.Drawing.Point(514, 325);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 11;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Image = ((System.Drawing.Image)(resources.GetObject("CloseButton.Image")));
+            this.CloseButton.ImageHover = ((System.Drawing.Image)(resources.GetObject("CloseButton.ImageHover")));
+            this.CloseButton.ImageNormal = ((System.Drawing.Image)(resources.GetObject("CloseButton.ImageNormal")));
+            this.CloseButton.Location = new System.Drawing.Point(563, 9);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(27, 34);
+            this.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CloseButton.TabIndex = 13;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
             // WorkOrderModuleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(800, 443);
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.labelamount);
             this.Controls.Add(this.lablePrice);
             this.Controls.Add(this.Item4);
@@ -197,13 +223,14 @@
             this.Controls.Add(this.LableClientName);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "WorkOrderModuleForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WorkOrderModuleForm";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.MyLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,5 +250,8 @@
         private System.Windows.Forms.Label lablePrice;
         private System.Windows.Forms.Label labelamount;
         public System.Windows.Forms.Label lableRentalOption;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
--- InventoryManagmentSystem/WorkOrderModuleForm.cs
+++ InventoryManagmentSystem/WorkOrderModuleForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -90,6 +91,62 @@ namespace InventoryManagmentSystem
             if (MessageBox.Show("Exit Module?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
                 this.Dispose();
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            /*// Show the print dialog
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            // Create a PrintDocument and set its properties
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.DocumentName = "My Form";
+            printDocument.PrinterSettings = printDialog.PrinterSettings;
+            printDocument.DefaultPageSettings.Landscape = true; // Example page setting
+            printDocument.PrintPage += PrintPage;
+
+            // Print the document
+            printDocument.Print();*/
+
+        /*    // Create a new PrintDocument object
+            PrintDocument pd = new PrintDocument();
+
+            // Set the PrintPage event handler
+            pd.PrintPage += new PrintPageEventHandler(this.PrintPage);
+
+            // Show the print dialog and start the print job
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pd.PrinterSettings = printDialog1.PrinterSettings;
+                pd.Print();
+            }*/
+
+            PrintDialog printDialog1 = new PrintDialog();
+            printDialog1.Document = printDocument1;
+
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+        }
+
+        private void PrintPage(object sender, PrintPageEventArgs e)
+        {
+            try
+            {
+                if(this.BackgroundImage != null)
+                {
+                    // Draw the contents of the form onto the PrintPageEventArgs
+                    this.DrawToBitmap(new Bitmap(this.Width, this.Height), new Rectangle(Point.Empty, this.Size));
+                    e.Graphics.DrawImageUnscaled(this.BackgroundImage, this.Location);  
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
