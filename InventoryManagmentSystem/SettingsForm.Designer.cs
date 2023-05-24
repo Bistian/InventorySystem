6e82a437b5e8985e86d68f6f5f22b1f9c128b1ea
FreyLF <fernandoo_b@hotmail.com>
2023-05-24 12:09:37 -0300

    SettingsForm -Forgot to push

---
+++ InventoryManagmentSystem/SettingsForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace InventoryManagmentSystem
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnDatabase = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnDatabase
+            // 
+            this.btnDatabase.Location = new System.Drawing.Point(13, 42);
+            this.btnDatabase.Name = "btnDatabase";
+            this.btnDatabase.Size = new System.Drawing.Size(75, 23);
+            this.btnDatabase.TabIndex = 1;
+            this.btnDatabase.Text = "Database";
+            this.btnDatabase.UseVisualStyleBackColor = true;
+            this.btnDatabase.Click += new System.EventHandler(this.btnDatabase_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(121, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(667, 426);
+            this.panel1.TabIndex = 2;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnDatabase);
+            this.Controls.Add(this.btnImport);
+            this.Name = "SettingsForm";
+            this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnDatabase;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ InventoryManagmentSystem/SettingsForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagmentSystem
+{
+    public partial class SettingsForm : Form
+    {
+        //to show subform in mainform
+        private Form activeForm = null;
+
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        public void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel1.Controls.Add(childForm);
+            panel1.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            openChildForm(new ExcelImportForm());
+        }
+
+        private void btnDatabase_Click(object sender, EventArgs e)
+        {
+            openChildForm(new DatabaseCreationModule(false));
+        }
+    }
+}
