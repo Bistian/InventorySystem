1a10d7510037ce02e698fb3e23ff5fbe5340e1ee
FreyLF <fernandoo_b@hotmail.com>
2023-10-04 19:39:26 -0300

    Class roster

---
+++ InventoryManagmentSystem/Academy/ClassRosterForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagmentSystem.Academy
+{
+    public partial class ClassRosterForm : Form
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        AcademyForm parent;
+        public ClassRosterForm(AcademyForm parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            string query = $"SELECT DriversLicense, Name, Phone, Email FROM tbClients WHERE IdClass = '{parent.ClassId}'";
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                int index = 0;
+                while (reader.Read())
+                {
+                    ++index;
+                    dataGridStudents.Rows.Add(index, reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+        }
+    }
+}
