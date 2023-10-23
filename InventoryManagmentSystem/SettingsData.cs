06504c466a6134e1d8d8b76f7c7e1eec29c854e0
FreyLF <fernandoo_b@hotmail.com>
2023-10-23 13:36:52 -0300

    Settings files

---
+++ InventoryManagmentSystem/SettingsData.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagmentSystem
+{
+    internal class SettingsData
+    {
+        private static SettingsData instance;
+        private string filePath;
+        /// <summary>
+        /// If a variable has get or set, it will be added to the config file. 
+        /// Add variables that are not to be tracked here.
+        /// </summary>
+        private Dictionary<string, bool> untrackedProperties = new Dictionary<string, bool> {
+            // Add more variables to be left out of the config file.
+            {"Instance", true },
+        };
+        /// <summary>
+        /// For a variable to be written on the config file, it needs to be here with its default value.
+        /// </summary>
+        private Dictionary<string, string> defaultData = new Dictionary<string, string>
+        {
+            // Add more variables here
+            { "dueDaysFromToday", "14" },
+            { "fontSize", "12" },
+        };
+        /// <summary>
+        /// Current values of settings' variables.
+        /// </summary>
+        public Dictionary<string, string> currentData;
+        /// <summary>
+        /// Used to filter items based on their due date in relation to today.
+        /// </summary>
+        public int dueDaysFromToday { get; set; }
+        /// <summary>
+        /// Size of the letters.
+        /// </summary>
+        public float fontSize { get; set; }
+        
+        public static SettingsData Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new SettingsData();
+                    string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                    string fileName = "config.txt";
+                    instance.filePath = Path.Combine(baseDirectory, fileName);
+                    instance.UpdateFieldsUsingTextFile();
+                }
+                return instance;
+            }
+        }
+
+        private bool CreateSettingsFile()
+        {
+            try
+            {
+                // Create or overwrite the text file at the specified path
+                using (StreamWriter writer = File.CreateText(filePath))
+                {
+                    // Write the data in the specified format
+                    foreach (var kvp in defaultData)
+                    {
+                        writer.WriteLine($"{kvp.Key}: {kvp.Value}");
+                    }
+                }
+                return true;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return false;
+            }
+        }
+
+        private Dictionary<string, string> ReadTextFile()
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            try
+            {
+                // Read the text file and parse the lines
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(new char[] { ':' }, 2, StringSplitOptions.None);
+                    if (parts.Length == 2)
+                    {
+                        string variableName = parts[0].Trim();
+                        string variableValue = parts[1].Trim();
+                        data[variableName] = variableValue;
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return data;
+        }
+       
+        public void RevertToDefault()
+        {
+            currentData = defaultData;
+            UpdateSettingsFile(defaultData);
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                UpdateCurrentData();
+                UpdateSettingsFile(currentData);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void UpdateCurrentData()
+        {
+            Dictionary<string, string> updatedData = new Dictionary<string, string>();
+            Type type = typeof(SettingsData);
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (untrackedProperties.ContainsKey(property.Name))
+                {
+                    continue;
+                }
+                string propertyName = property.Name;
+                object propertyValue = property.GetValue(this);
+                updatedData[propertyName] = propertyValue.ToString();
+            }
+            currentData = updatedData;
+        }
+
+        /// <summary>
+        /// Goes through all variables of SettingsData and set their values based on the given dictionary.
+        /// </summary>
+        /// <param name="data">Dictionary with variable names and values.</param>
+        private void UpdateFields(Dictionary<string, string> data)
+        {
+            Type type = typeof(SettingsData);
+            foreach (var kvp in data)
+            {
+                PropertyInfo property = type.GetProperty(kvp.Key);
+                if (property != null)
+                {
+                    object value = Convert.ChangeType(kvp.Value, property.PropertyType);
+                    property.SetValue(this, value);
+                }
+            }
+        }
+        
+        private void UpdateFieldsUsingTextFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                if (!CreateSettingsFile())
+                {
+                    return;
+                }
+            }
+
+            // TODO: Update existing file if new fields are added.
+
+            currentData = ReadTextFile();
+            UpdateFields(currentData);
+        }
+
+        private void UpdateSettingsFile(Dictionary<string, string> data)
+        {
+            try
+            {
+                // Create or overwrite the text file at the specified path
+                using (StreamWriter writer = File.CreateText(filePath))
+                {
+                    // Write the data in the specified format
+                    foreach (var kvp in data)
+                    {
+                        writer.WriteLine($"{kvp.Key}: {kvp.Value}");
+                    }
+                }
+
+                Console.WriteLine("Text file updated with new data.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ InventoryManagmentSystem/ToolsForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagmentSystem
+{
+    public partial class ToolsForm : Form
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        // Colors
+        Color offColor = Color.Transparent;
+        Color onColor = Color.DarkGoldenrod;
+
+        //to show subform in mainform
+        private Form activeForm = null;
+
+        public ToolsForm()
+        {
+            InitializeComponent();
+            devInitAddItem();
+        }
+
+        public void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel1.Controls.Add(childForm);
+            panel1.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void ColorTabSwitch(string tab)
+        {
+            // Set all colors to normal.
+            btnDatabase.BackColor = offColor;
+            btnImport.BackColor = offColor;
+            btnBrands.BackColor = offColor;
+            btnPrices.BackColor = offColor;
+
+            // Pick one tab and set it to the clicked color.
+            if (tab == "Database") { btnDatabase.BackColor = onColor; }
+            else if (tab == "Import") { btnImport.BackColor = onColor; }
+            else if (tab == "Brands") { btnBrands.BackColor = onColor; }
+            else if (tab == "Prices") { btnPrices.BackColor = onColor; }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ColorTabSwitch("Import");
+            openChildForm(new ExcelImportForm());
+        }
+
+        private void btnDatabase_Click(object sender, EventArgs e)
+        {
+            ColorTabSwitch("Database");
+            openChildForm(new DatabaseCreationModule(false));
+        }
+
+        private void btnBrands_Click(object sender, EventArgs e)
+        {
+            ColorTabSwitch("Brands");
+            openChildForm(new BrandForm());
+        }
+
+        private void btnPrices_Click(object sender, EventArgs e)
+        {
+            ColorTabSwitch("Prices");
+            openChildForm(new PricesForm());
+        }
+
+        private void devInitAddItem()
+        {
+#if DEBUG
+            cbItemType.Enabled = true;
+            cbItemType.Visible = true;
+            tbUuid.Enabled = true;
+            tbUuid.Visible = true;
+            btnAddItem.Enabled = true;
+            btnAddItem.Visible = true;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            HelperDatabaseCall.ItemTypeLoadComboBox(connection, cbItemType);
+#endif
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            if(cbItemType.SelectedIndex < 0) { return; }
+            if(!HelperFunctions.YesNoMessageBox("Do you want to add this Item?", "Add Item")) { return; }
+
+            string table = "";
+            if(cbItemType.Text == "boots") { table = "tbBoots"; }
+            else if (cbItemType.Text == "helmet") { table = "tbHelmets"; }
+            else if (cbItemType.Text == "jacket") { table = "tbJackets"; }
+            else if (cbItemType.Text == "mask") { table = "tbMasks"; }
+            else if (cbItemType.Text == "pants") { table = "tbPants"; }
+            else { return; }
+
+            // Loop through the table.
+            while(true)
+            {
+                Guid uuid = HelperDatabaseCall.ItemInsertAndGetUuid(connection, cbItemType.Text);
+                // Add item id to null.
+                string query = $@"
+                    UPDATE TOP(1) {table}
+                    SET ItemId = '{uuid}'
+                    OUTPUT 1 
+                    WHERE ItemId IS NULL;
+                ";
+                HelperFunctions.RemoveLineBreaksFromString(ref query);
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery(); connection.Close();
+                    if(rowsAffected < 1) 
+                    { 
+                        HelperDatabaseCall.ItemDelete(connection, uuid);
+                        break; 
+                    }
+                }
+                catch (Exception ex)
+                {
+                    HelperDatabaseCall.ItemDelete(connection, uuid);
+                    Console.WriteLine(ex.Message);
+                    connection.Close();
+                }
+            }
+            Console.WriteLine("Gaje likes minors!");
+        }
+    }
+}
